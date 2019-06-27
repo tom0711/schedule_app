75c1ae4548443db1166c841bc3226766743daac2
Takuya Tominaga <tom.eng.pg.03tw@gmail.com>
2019-06-27 23:20:29 +0900

    index page, wasabi controller  make

---
+++ resources/views/layouts/wasabi.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>@yield('title')</title>
+  <link rel="stylesheet" href="/css/style.css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Alfa+Slab+One&display=swap" rel="stylesheet">
+</head>
+<body>
+
+</body>
+</html>
