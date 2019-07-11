3612f9879a610169f261ac8a85abec4073d246ec
Takuya Tominaga <tom.eng.pg.03tw@gmail.com>
2019-07-11 19:32:16 +0900

    make dialog space

--- resources/views/inquiry/index.blade.php
+++ resources/views/inquiry/index.blade.php
@@ -24,4 +24,36 @@
         @endforeach
     </table>
   </div>
+  <div class="none"  style="background-color:yellow;">
+    <table>
+      @foreach($items as $item)
+        @if($item->user_id == $id)
+          <tr>
+            <th>問い合わせ日時</th>
+          </tr>
+          <tr>
+            <td>{{$item->date}}</td>
+          </tr>
+          <tr>
+            <th>タイトル</th>
+          </tr>
+          <tr>
+            <td>{{$item->title}}</td>
+          </tr>
+          <tr>
+            <th>問い合わせ内容</th>
+          </tr>
+          <tr>
+            <td>{{$item->question}}</td>
+          </tr>
+          <tr>
+            <th>回答</th>
+          </tr>
+          <tr>
+            <td>{{$item->answer}}</td>
+          </tr>
+        @endif
+      @endforeach
+    </table>
+  </div>
 @endsection
---
+++ resources/views/none.blade.php
@@ -0,0 +1,11 @@
+@extends('home')
+
+@section('none')
+<div class="">
+  <table style="background-color:yellow;">
+    <tr>
+      <th>a</th>
+    </tr>
+  </table>
+</div>
+@endsection
